e609cea2071c1b62855aa55e881365afbe912b53
xiaozhai801 <zhaimou81@qq.com>
2026-04-28 20:12:25 +0800

    更新主题

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,164 @@
+import { defineConfig } from "vitepress";
+import llmstxt from "vitepress-plugin-llms";
+import { teekConfig } from "./teekConfig";
+
+const description = [
+  "欢迎来到 vitepress-theme-teek 使用文档",
+  "Teek 是一个基于 VitePress 构建的主题，是在默认主题的基础上进行拓展，支持 VitePress 的所有功能、配置",
+  "Teek 拥有三种典型的知识管理形态：结构化、碎片化、体系化，可以轻松构建一个结构化知识库，适用个人博客、文档站、知识库等场景",
+].toString();
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  extends: teekConfig,
+  title: "ZZZ的个人博客",
+  description: description,
+  cleanUrls: false,
+  lastUpdated: true,
+  lang: "zh-CN",
+  head: [
+    [
+      "link",
+      { rel: "icon", type: "image/svg+xml", href: "/teek-logo-mini.svg" },
+    ],
+    ["link", { rel: "icon", type: "image/png", href: "/teek-logo-mini.png" }],
+    ["meta", { property: "og:type", content: "website" }],
+    ["meta", { property: "og:locale", content: "zh-CN" }],
+    ["meta", { property: "og:title", content: "Teek | VitePress Theme" }],
+    ["meta", { property: "og:site_name", content: "Teek" }],
+    ["meta", { property: "og:image", content: "" }],
+    ["meta", { property: "og:url", content: "" }],
+    ["meta", { property: "og:description", description }],
+    ["meta", { name: "description", description }],
+    ["meta", { name: "author", content: "Teek" }],
+    // 禁止浏览器缩放
+    // [
+    //   "meta",
+    //   {
+    //     name: "viewport",
+    //     content: "width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no",
+    //   },
+    // ],
+    ["meta", { name: "keywords", description }],
+  ],
+  markdown: {
+    // 开启行号
+    lineNumbers: true,
+    image: {
+      // 默认禁用；设置为 true 可为所有图片启用懒加载。
+      lazyLoading: true,
+    },
+    // 更改容器默认值标题
+    container: {
+      tipLabel: "提示",
+      warningLabel: "警告",
+      dangerLabel: "危险",
+      infoLabel: "信息",
+      detailsLabel: "详细信息",
+    },
+  },
+  sitemap: {
+    hostname: "https://xiaozhai801.github.io", // ** 换成你的域名
+    transformItems: (items) => {
+      const permalinkItemBak: typeof items = [];
+      // 使用永久链接生成 sitemap
+      const permalinks = (globalThis as any).VITEPRESS_CONFIG.site.themeConfig
+        .permalinks;
+      items.forEach((item) => {
+        const permalink = permalinks?.map[item.url];
+        if (permalink)
+          permalinkItemBak.push({ url: permalink, lastmod: item.lastmod });
+      });
+      return [...items, ...permalinkItemBak];
+    },
+  },
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: "/teek-logo-mini.svg",
+    darkModeSwitchLabel: "主题",
+    sidebarMenuLabel: "菜单",
+    returnToTopLabel: "返回顶部",
+    lastUpdatedText: "上次更新时间",
+    outline: {
+      level: [2, 4],
+      label: "本页导航",
+    },
+    docFooter: {
+      prev: "上一页",
+      next: "下一页",
+    },
+    nav: [
+      { text: "首页", link: "/" },
+      {
+        text: "指南",
+        link: "/guide/intro",
+        activeMatch: "/01.指南/",
+      },
+      { text: "配置", link: "/reference/config", activeMatch: "/10.配置/" },
+      { text: "开发", link: "/develop/intro", activeMatch: "/15.主题开发/" },
+      {
+        text: "功能页",
+        items: [
+          { text: "归档页", link: "/archives" },
+          { text: "清单页", link: "/articleOverview" },
+          { text: "登录页", link: "/login" },
+          {
+            text: "风险链接提示页",
+            link: "/risk-link?target=https://vp.teek.top",
+          },
+          { text: "分类页", link: "/categories" },
+          { text: "标签页", link: "/tags" },
+        ],
+      },
+      { text: "✨ 赞赏", link: "/personal/" },
+    ],
+    socialLinks: [
+      {
+        icon: "github",
+        link: "https://github.com/Kele-Bingtang/vitepress-theme-teek",
+      },
+    ],
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          root: {
+            translations: {
+              button: {
+                buttonText: '搜索',
+                buttonAriaLabel: '搜索'
+              },
+              modal: {
+                displayDetails: '显示详细列表',
+                resetButtonTitle: '重置搜索',
+                backButtonTitle: '关闭搜索',
+                noResultsText: '没有结果',
+                footer: {
+                  selectText: '选择',
+                  selectKeyAriaLabel: '输入',
+                  navigateText: '导航',
+                  navigateUpKeyAriaLabel: '上箭头',
+                  navigateDownKeyAriaLabel: '下箭头',
+                  closeText: '关闭',
+                  closeKeyAriaLabel: 'Esc'
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    editLink: {
+      text: "在 GitHub 上编辑此页",
+      pattern:
+        "https://github.com/Kele-Bingtang/vitepress-theme-teek/edit/master/docs/:path",
+    },
+  },
+  vite: {
+    plugins: [llmstxt() as any],
+  },
+  // transformHtml: (code, id, context) => {
+  //   if (context.page !== "404.md") return code;
+  //   return code.replace("404 | ", "");
+  // },
+});
---
+++ docs/index.md
@@ -0,0 +1,42 @@
+---
+layout: home
+
+hero:
+  name: Teek
+  text: VitePress 主题
+  tagline: ✨一个轻量、简洁高效、灵活配置，易于扩展的 VitePress 主题
+  actions:
+    - theme: brand
+      text: 开始
+      link: /guide/quickstart
+    - theme: alt
+      text: 配置
+      link: /reference/config
+  image:
+    src: /teek-logo-large.png
+    alt: Teek
+
+features:
+  - icon: 📖
+    title: 结构化 && 体系化
+    details: 自动生成侧边栏、目录页、索引页、面包屑等，轻松构建一个结构化知识库。
+  - icon: 🎉
+    title: 碎片化 & 个性化
+    details: 提供快速构建知识的碎片化形态，并提供大量个性化的主题配置。
+  - icon: 🎇
+    title: 文档风 & 博客风
+    details: 支持通过配置搭配出各种风格，支持个人博客、文档站、知识库等场景。
+  - icon: 📝
+    title: 专注内容
+    details: 只需 Markdown 即可轻松创建美观的文档站点，配合多维索引快速定位每个知识点。
+---
+
+## 风格切换
+
+Teek 可以通过配置搭配出各种风格，而当前文档站默认仅演示较简约的文档风格。
+
+在右上角 <TkIcon :icon="readingIcon" :size="20" /> 图标的 <mark>配置切换</mark> 区域可以切换其他风格。因为配置较多，很多的功能并没有完全演示，需要安装 Teek，然后自行探索出自己喜欢的风格。
+
+## 💡 反馈交流
+
+在使用过程中有任何问题和想法，请给我提 [Issue](https://github.com/Kele-Bingtang/vitepress-theme-teek/issues)。 你也可以在 Issue 查看别人提的问题和解决方案。
